f41faf68889f6628ea218667264d440cff40c4b6
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-04-17 11:59:03 -0300

    vendo string e boolean

---
+++ 06-string/js/scripts.js
@@ -0,0 +1,26 @@
+var nome = "Gustavo";
+var sobrenome = "Gonçalves";
+
+var nome_completo = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asdhbgrtdf");
+
+console.log(nome_completo);
+
+console.log(typeof "5.345233");
+
+var frase = "esta é uma frase complexa";
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse: "Olá"');
+
+document.write("Ele disse: 'Olá'");
+
+console.log("Este número: " + nome);
