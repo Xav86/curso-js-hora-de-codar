139e0d6a581304ccfa623af4cf52ecba51539ca1
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-04-17 15:18:38 -0300

    operadores lógicos

---
+++ 14-op-comparacao-p2/js/scripts.js
@@ -0,0 +1,11 @@
+var numero = 4;
+
+if (numero === '5') {
+    console.log("O numero é 5");
+
+}
+
+if (numero !== 5) {
+    console.log("O numero não é 5 ou do tipo number");
+
+}
---
+++ 17-operador-logico-not/js/scripts.js
@@ -0,0 +1,9 @@
+if (!true){
+    console.log("Passou");
+}
+
+var nome = "Gustavo";
+
+if (!(nome == "Guustavo")){
+    console.log("Ok");
+}
