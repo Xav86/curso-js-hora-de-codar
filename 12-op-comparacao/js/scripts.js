eb953d97afd15db2a0c6a4779f0e8c43e7175dc4
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-04-17 14:20:54 -0300

    if, else if e else

---
+++ 12-op-comparacao/js/scripts.js
@@ -0,0 +1,38 @@
+var idade = 18;
+var possuiCarro = 1; //0 = false / 1 = true
+
+if(idade >= 18) {
+    console.log("O usuário pode fazer a carteira");
+
+}
+
+if (idade <= 17) {
+    console.log("O usuário não pode fazer a carteira");
+
+}
+
+if(possuiCarro){
+    console.log("O usuário ja pode andar de carro");
+
+}
+
+var nome = "Gustavo";
+
+if (nome == "Gustavo"){
+    console.log("O seu nome é Gustavo");
+    
+}
+
+if (nome != "Gustavo"){
+    console.log("O seu nome não é Gustavo");
+    
+}
+
+if (20 > 10){
+    console.log("Passou");
+
+}
+
+if (100 < 20) {
+    console.log("Passou 2");
+}
---
+++ 13-else-elseif/js/scripts.js
@@ -0,0 +1,23 @@
+var nome = "Gustavo";
+
+if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+}else if("Gustavo"){
+    console.log("O nome é Gustavo");
+}else{
+    console.log("Ele possui outro nome");
+}
+
+var idade = 19;
+
+if (idade > 20){
+    console.log("Ele pode entrar na festa!");
+}else if(idade >= 18){
+    console.log("Ele só pode entrar com autorização");
+}
+
+if (nome == "Gustavo"){
+    console.log("Verdade");
+} else {
+    console.log("Mentira");
+}
