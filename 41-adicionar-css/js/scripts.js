87a0221e5f49bd8672203a35a25d3d33c334316e
Gustavo Gonçalves dos Santos <gustavo.santos@fontana.local>
2024-05-13 09:19:57 -0300

    criar/substituir elementos, alterar atributo e adicionar css

---
+++ 41-adicionar-css/js/scripts.js
@@ -0,0 +1,14 @@
+// seleciona elemento
+var title = document.querySelector("#title");
+
+// adiciona o elemento
+title.style.color = "red";
+
+// background-color (palavras com "-" tem que transformar em uma palavra só)
+title.style.backgroundColor = "yellow";
+
+// selecionar elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 12px;";
